eb6f31f4502f452e9cd302aa9c3836dcbe4227c6
Vikas Pogu <vikaspoguadf@gmail.com>
2025-10-02 09:46:52 -0400

    (fix): remove region

--- terraform/authentik/backend.tf
+++ terraform/authentik/backend.tf
@@ -2,12 +2,11 @@ terraform {
   backend "s3" {
     bucket = "tofu-state"
     key    = "authentik/authentik.tfstate"
-    region = "main" # Region validation will be skipped
 
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     skip_metadata_api_check     = true
-    skip_region_validation      = true
+    skip_region_validation      = false
     use_path_style              = true
   }
 }
